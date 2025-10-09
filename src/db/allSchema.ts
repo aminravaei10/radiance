6a7c55d5e17ca247d8eabc3caf25fac72d035fe7
aminravaei10 <amin.ravaei10@gmil.com>
2025-10-09 18:50:24 +0330

    fix: add coulmn log-id in file schema

--- src/db/allSchema.ts
+++ src/db/allSchema.ts
@@ -2,4 +2,4 @@ import { userModel } from 'src/user/schema/user.schema';
 import { fileModel } from 'src/file/schema/file.schema';
 import { tokenModel } from 'src/ai/schema/token.schema';
 import { userLogModel } from 'src/user/schema/user-log';
-export { fileModel as files, userModel, tokenModel, userLogModel };
+export { fileModel, userModel, tokenModel, userLogModel };
